a0a8c04275b7942a8d48c7732d129228578a19e2
Xavier Torgerson <xaviertorgerson@gmail.com>
2017-02-10 22:30:36 -0500

    BTC-e and Bitstamp getters

--- prototypes/get_market_prices.py
+++ prototypes/get_market_prices.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 headers = {'User-Agent': 'Mozilla/5.0'}
 BITFINEX_URL = "https://www.bitfinex.com/stats"
 
@@ -17,21 +18,37 @@ def get_bitfinex():
                 marketPrice = float(nextSiblingElement.text)
 
     return marketPrice
-    
+
+BTCE_URL = "https://btc-e.com"
+def get_btce():
+    r = requests.get(BTCE_URL, headers=headers).text
+    soup = BeautifulSoup(r, "html.parser")
+    strongList = soup.select("strong")
+    price = re.findall("[0-9]+.[0-9]+", str(strongList[0]))
+    return price[0]
+
+BITSTAMP_URL = "https://bitcoinwisdom.com"
+def get_bitstamp():
+    r = requests.get(BITSTAMP_URL, headers=headers).text
+    soup = BeautifulSoup(r, "html.parser")
+    span = soup.find("span", {"id": "market_bitstampbtcusd"})
+    price = re.findall("[0-9]+.[0-9]+", str(span))
+    return price[0]
+
 #The method will return the current market price for a given exchange
 #exchangeName - a string that specifies the exchange
 #returns the current market price as a float
 def handler(exchangeName):
-    marketPrice = 0.0
     if (exchangeName == "bitfinex"):
-        marketPrice = get_bitfinex()
-        return marketPrice
-    elif (exchangeName == ""):
-        return 0.0
-    elif (exchangeName == "okcoin"):
-        return 0.0
+        return get_bitfinex()
+    elif (exchangeName == "btce"):
+        return get_btce()
+    elif (exchangeName == "bitstamp"):
+        return get_bitstamp()
     else:
         print "Unrecognized exchange"
-        return 0.0
+        return 0
 
 print("Bitfinex market price: "+str(handler("bitfinex")))
+print("BTC-e market price: "+str(handler("btce")))
+print("Bitstamp market price: "+str(handler("bitstamp")))
