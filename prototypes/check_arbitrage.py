0aa8d27ba83b70def7fa8df7feb661544eb6d96d
BDANG <brindang776@aol.com>
2017-02-11 00:12:18 -0500

    including fees in arb. need to consider withdraw fees

--- prototypes/check_arbitrage.py
+++ prototypes/check_arbitrage.py
@@ -2,6 +2,35 @@ import itertools
 import requests
 import get_market_prices
 
+BUY_FEES_DICT = {} #fees represented as a decimal
+BUY_FEES_DICT["bitfinex"] = .002
+BUY_FEES_DICT["bitstamp"] = .0025
+BUY_FEES_DICT["btce"] = .005
+BUY_FEES_DICT["cexio"] = .002
+
+SELL_FEES_DICT = {} #fees represented as a decimal
+SELL_FEES_DICT["bitfinex"] = .002
+SELL_FEES_DICT["bitstamp"] = .0025
+SELL_FEES_DICT["btce"] = .005
+SELL_FEES_DICT["cexio"] = .002
+
+#The function calculates the arbritrage including fees from the exchanges
+#marketPriceDict - a dictionary that maps exchange name to current market price
+#exchange1 - an exchange that user is BUYING BTC on
+#exchange2 - an exchange that user is SELLING BTC on
+#!!!CURRENTLY ASSUMES FEE IS APPLIED TO CURRENCY RECIEVED (RECIEVE LESS THAN EXPECTED)
+def arbitrage_difference_withfees(marketPriceDict, exchange1, exchange2):
+    buyFee = BUY_FEES_DICT[exchange1]
+    sellFee = SELL_FEES_DICT[exchange2]
+
+    buyPrice = marketPriceDict[exchange1] * (1.0+buyFee)
+    sellPrice = marketPriceDict[exchange2] * (1.0-sellFee)
+
+    #%profit difference = (price sold - price bought) / price bought
+    arbitragePercent = (sellPrice - buyPrice) / buyPrice
+    return arbitragePercent
+
+
 
 #The function calculates the potential arbritrage between two exchanges
 #marketPriceDict - a dictionary that maps exchange name to current market price
@@ -15,17 +44,21 @@ def calculate_arbitrage(marketPriceDict, exchange1, exchange2):
     market1 = marketPriceDict[exchange1]
     market2 = marketPriceDict[exchange2]
     arbDiff = 0.0 # a percent representing the arbritrage
+    arbitragePercentWithFee = 0.0
     maxMarket = max(market1, market2)
-    if (maxMarket == market1): #market1 and exchange1 is the larger price
+    if (maxMarket == market1): #market1 and exchange1 is the larger price and SOLD
         arbDiff = market1/market2 #WIP change to include fees
+        arbitragePercentWithFee = arbitrage_difference_withfees(marketPriceDict, exchange2, exchange1)
         buyExchange = exchange2
         sellExchange = exchange1
     else: #market2 and exchange2 is the larger price
         arbDiff = market2/market1 #WIP change to include fees
+        arbitragePercentWithFee = arbitrage_difference_withfees(marketPriceDict, exchange1, exchange2)
         buyExchange = exchange1
         sellExchange = exchange2
 
     print "\nThe arbitrage difference is: "+str((arbDiff-1)*100.0)
+    print "Including fee, the difference is: "+str(arbitragePercentWithFee*100.0)
     print "Buy on exchange: "+buyExchange+" ("+str(marketPriceDict[buyExchange])+")"
     print "Sell on exchange: "+sellExchange+" ("+str(marketPriceDict[sellExchange])+")\n"
 
@@ -47,6 +80,5 @@ def handler(listOfExchanges):
 
 
 
-
-TESTLIST = ["bitfinex", "btce", "bitstamp", "cexio"]
+TESTLIST = ["bitfinex", "btce", "bitstamp", "cexio", "kraken"]
 handler(TESTLIST)
