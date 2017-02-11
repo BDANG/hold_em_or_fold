1a2a6c57321a1f7abac10dfa0d1588b98f305bd7
Xavier Torgerson <xaviertorgerson@gmail.com>
2017-02-11 00:07:45 -0500

    Kraken API

--- prototypes/get_market_prices.py
+++ prototypes/get_market_prices.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-headers = {'User-Agent': 'Mozilla/5.0'}
-BITFINEX_URL = "https://www.bitfinex.com/stats"
+import json
+import krakenex
 
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+k = krakenex.API()
+k.load_key('kraken.key')
+
+
+
+BITFINEX_URL = "https://www.bitfinex.com/stats"
 def get_bitfinex():
     marketPrice = 0.0
     r = requests.get(BITFINEX_URL, headers=headers).text
@@ -48,8 +56,15 @@ def get_itbit():
     r = requests.get(ITBIT_URL, headers=headers).text
     soup = BeautifulSoup(r, "html.parser")
     span = soup.find("span", {"id": "last"})
+    print(span)
     price = re.findall("[0-9]+.[0-9]+", str(span))
     return float(price[0])
+
+def get_kraken():
+    json_string = k.query_public('Ticker', {'pair': 'XXBTZUSD'})
+    ticker_list = re.findall("[0-9]+.[0-9]+", str(json_string))
+    return ticker_list[2]
+
 #The method will return the current market price for a given exchange
 #exchangeName - a string that specifies the exchange
 #returns the current market price as a float
@@ -63,13 +78,17 @@ def handler(exchangeName):
     elif (exchangeName == "cexio"):
         return get_cexio()
     elif (exchangeName == "itbit"):
-        return get_cexio()
+        return get_itbit()
+    elif (exchangeName == "kraken"):
+        return get_kraken()
     else:
         print "Unrecognized exchange"
         return 0
+
 
 print("Bitfinex market price: "+str(handler("bitfinex")))
 print("BTC-e market price: "+str(handler("btce")))
 print("Bitstamp market price: "+str(handler("bitstamp")))
 print("CEX.io market price: "+str(handler("cexio")))
-print("itBit market price: "+str(handler("itbit")))
+#print("itBit market price: "+str(handler("itbit")))
+print("Kraken market price: "+str(handler("kraken")))
