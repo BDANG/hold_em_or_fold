456ae4556483e169ee54aa9b8134071166967428
BDANG <brindang776@aol.com>
2017-02-10 21:32:19 -0500

    working get_market_price(bitfinex)

--- prototypes/get_market_prices.py
+++ prototypes/get_market_prices.py
@@ -1,21 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 headers = {'User-Agent': 'Mozilla/5.0'}
-BITFINEX_URL = "https://www.bitfinex.com"
+BITFINEX_URL = "https://www.bitfinex.com/stats"
+
 def get_bitfinex():
-    print "okay"
+    marketPrice = 0.0
     r = requests.get(BITFINEX_URL, headers=headers).text
     soup = BeautifulSoup(r, "html.parser")
-    spans = soup.select("span")
-    print spans
-    return 0.0
+
+    #btc price within a <td> element
+    tdList = soup.select("td")
+    for tdElement in tdList: #iterate the list of <td>
+        if tdElement.text == "BTCUSD": #there are two: <td>BTCUSD</td>
+            nextSiblingElement = tdElement.next_sibling.next_sibling
+            if ("\n" not in nextSiblingElement.text):
+                marketPrice = float(nextSiblingElement.text)
+
+    #print spans
+    #print r
+
+    return marketPrice
 #The method will return the current market price for a given exchange
 #exchangeName - a string that specifies the exchange
 #returns the current market price as a float
 def handler(exchangeName):
+    marketPrice = 0.0
     if (exchangeName == "bitfinex"):
-        x = get_bitfinex()
-        return 0.0
+        marketPrice = get_bitfinex()
+        return marketPrice
     elif (exchangeName == ""):
         return 0.0
     elif (exchangeName == "okcoin"):
@@ -24,4 +36,4 @@ def handler(exchangeName):
         print "Unrecognized exchange"
         return 0.0
 
-handler("bitfinex")
+print("Bitfinex market price: "+str(handler("bitfinex")))
