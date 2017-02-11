a10e6447f675e27a27495fd2fe8b39af83d85c7d
BDANG <brindang776@aol.com>
2017-02-11 17:25:17 -0500

    analysis will check the future price on the sell exchange to ensure profit security

--- backtest/historical_analysis.py
+++ backtest/historical_analysis.py
@@ -1,3 +1,5 @@
+import pickle
+import numpy
 import datetime
 import pandas
 import itertools
@@ -10,6 +12,7 @@ CSV_DIR = "/Users/briandang/Downloads/" #location of csv data
 EXCHANGE_LIST = ["bitfinex", "bitstamp", "btce", "kraken"] #make sure you have csv for each exchange
 THRESHOLD = .07 #a decimal representing the percent of the arbitrage
 SKIPS = 4 #skip every nth row in each exchange pandas.DataFrame()
+FUTURE_CHECK = 3600 #how many seconds into the future to check if the sell exchange upheld price
 
 
 BUY_FEES_DICT = {'bitfinex': .002,
@@ -69,6 +72,7 @@ def arbitrage_difference_withfees(marketPriceDict, exchange1, exchange2):
 def find_arbitrage(df1, df1Exchange, df2, df2Exchange, threshold):
     innerJoined = pandas.merge(df1, df2, on='unixtime', how='inner')
     marketPriceDict = {}
+    global TOTAL_ARB
     for index, row in innerJoined.iterrows():
         marketPriceDict[df1Exchange] = float(row["price_x"])
         marketPriceDict[df2Exchange] = float(row["price_y"])
@@ -88,8 +92,56 @@ def find_arbitrage(df1, df1Exchange, df2, df2Exchange, threshold):
 
 
 
+#Method checks the forward/future price from an arbitrage opportunity
+#Profit is secured when the BTC on a buy exchange is moved to the sell exchange
+#and then sold
+#arbObject an Arbitrage object holding an arbitrage opportunity
+#pandaDF - the pandas.DataFrame() corresponding to the historical exchange data for the arbObject.sellExch
+#time in SECONDS of how far into the future to look
+def check_forward_price(arbObject, pandaDF, time):
+    allowableWindow = 240 #+/- the seconds for (arbObject.unixtime+time)
+    previousPrice = arbObject.sellExchPrice
+    futureTime = arbObject.unixtime+time
+    #timewindow = numpy.linspace(futureTime-allowableWindow, futureTime+allowableWindow, ((futureTime+allowableWindow)-((futureTime-allowableWindow))))
+    #print len(timewindow)
+    #print pandaDF.loc[pandaDF["unixtime"].isin(timewindow)]
+
+    minTimeDF = pandaDF['unixtime'] > futureTime-allowableWindow
+    #print minTimeDF
+    maxTimeDF = pandaDF['unixtime'] < futureTime+allowableWindow
+    #print timeDF
+    timeDF = pandaDF[minTimeDF & maxTimeDF]
+    try:
+        futurePrice = timeDF.iloc[0]['price']
+    except:
+        return False
+    if previousPrice<=futurePrice:
+        return True
+    else:
+        return False
 
 
+
+
+def load():
+    fTotalArbList = open("list_of_total_arbitrageobjects.dat", "rb")
+    totalArbList = pickle.load(fTotalArbList)
+    fTotalArbList.close()
+
+    fpandaDFdict = open("dict_of_pandaDF.dat", "rb")
+    pandaDFdict = pickle.load(fpandaDFdict)
+    fpandaDFdict.close()
+    return totalArbList, pandaDFdict
+
+
+def save(totalArbList, pandaDFdict):
+    fTotalArbList = open("list_of_total_arbitrageobjects.dat", "wb")
+    pickle.dump(totalArbList, fTotalArbList)
+    fTotalArbList.close()
+
+    fpandaDFdict = open("dict_of_pandaDF.dat", "wb")
+    pickle.dump(pandaDFdict, fpandaDFdict)
+    fpandaDFdict.close()
 
 
 
@@ -110,14 +162,24 @@ def main():
     for combo in combos:
         find_arbitrage(pandaDFdict[combo[0]], combo[0], pandaDFdict[combo[1]], combo[1], THRESHOLD)
 
+
     TOTAL_ARB.sort(key=lambda x: x.unixtime)
 
-    #DESIRED DATA
-    startingPortfolio = 100
+    #save(TOTAL_ARB, pandaDFdict)
+    #global TOTAL_ARB
+    #TOTAL_ARB, pandaDFdict = load()
+
+
     for arbObject in TOTAL_ARB:
+        if (check_forward_price(arbObject, pandaDFdict[arbObject.sellExch], FUTURE_CHECK)):
+            print arbObject.toString()
+            print arbObject.sellExch+" maintained price for secured profit.\n"
+
+
+    #DESIRED DATA
+    #for arbObject in TOTAL_ARB:
         #print arbObject.toString() #history of arbitrage
-        startingPortfolio *= (arbObject.arb+1)
-    print startingPortfolio
-    print TOTAL_ARB[0].toString()
+
+
 
 main()
