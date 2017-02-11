962ba9c718e4042db541ea8b36742901acf4bbcd
cam292 <cam292@pitt.edu>
2017-02-11 12:09:20 -0500

    Almost all message handling complete. Need to add bad input

--- prototypes/global_data.py
+++ prototypes/global_data.py
@@ -1,2 +1,2 @@
-USER_DICT = {}
+USER_DICT = {} #{"phone":"1+11111111",([exchange list],arbitrage)}
 EXCHANGE_LIST = ["bitfinex", "bitstamp", "cexio", "kraken"]
--- prototypes/handle_messages.py
+++ prototypes/handle_messages.py
@@ -1,8 +1,11 @@
 from twilio.rest import TwilioRestClient
 import global_data
+import time
 
-#global_data.USER_DICT = {'+11234567890':([0,1],0.0)} #customer dictionary. format "number":{"markets":[marketList],"minArgbitrage":float}
+global currentSid
+currentSid = "SM9781d28861123b6fa088a2192bd79858" #sid of last checked message
 
+#possible responses to user input
 RESPONSE_DICT = {"newCust":"Welcom to BAO. Choose the bitcoin markets you currently have assets in: 1)bitfinex 2)bitstamp 3)cexio 4)kraken. Reply 'stop' to opt out","minArb":"What would you like your minimum arbitrage threshold to be?(percentage) Reply 'stop' to opt out","thank":"Thank you for using BOA. You will be receiving alerts when there is a valid arbitrage available","stop":"Thank you for using BOA. Text 'Join' to 724-806-1286 if you would like to join again.","invalidNum":"You must enter a number. Please try again","invalidCommand":"The command you entered is invalid. Valid commands are: reset or stop."}
 
 accountSid = "AC410618cdbef7d152a5b5b265e70d06cb" # test account sid
@@ -16,100 +19,82 @@ def send_message(userNumber,message):
     body=message)
     return message.status
 
-#lists all messages
-def receive_messages():
-    messages = client.messages.list(to="+17248061286") #only get messages sent to us
-    return messages
-
 #parse user message
 #phoneNumber and message are strings
-def handle_message(phoneNumber,message):
-    if(message.lower() == "reset"): #user wants to select new markets
+def handle_message(message):
+    if(message.body.lower() == "reset"): #user wants to select new markets
         print "Reset"
-        #del global_data.USER_DICT[phoneNumber]
-        global_data.USER_DICT[phoneNumber] = ([],0.0) #reset values
-        send_message(phoneNumber,RESPONSE_DICT["newCust"])
+        global_data.USER_DICT[message.from_] = ([],0.0) #reset values
+        send_message(message.from_,RESPONSE_DICT["newCust"])
         return
 
-    elif(message.lower() == "stop"):
+    elif(message.body.lower() == "stop"): #twilio handles unsubscribe message
         print "Stop"
-        del global_data.USER_DICT[phoneNumber] #remove customer from dictionary
-        send_message(phoneNumber,RESPONSE_DICT["stop"])
+        del global_data.USER_DICT[message.from_] #remove customer from dictionary
         return
 
-    elif(phoneNumber not in global_data.USER_DICT): #new customer
+    elif(message.from_ not in global_data.USER_DICT): #new customer
         print "New user"
-        global_data.USER_DICT[phoneNumber] = ([],0.0)
-        send_message(phoneNumber,RESPONSE_DICT["newCust"]) #new customer message
+        global_data.USER_DICT[message.from_] = ([],0.0)
+
+        send_message(message.from_,RESPONSE_DICT["newCust"]) #new customer message
         return
 
-    elif(global_data.USER_DICT[phoneNumber][0] == []): #user message contains markets to follow
+    elif(global_data.USER_DICT[message.from_][0] == []): #user message contains markets to follow
         print "Get exchanges"
         exchanges = [] #temp list of exchanges from message
         valid = True
-        for i in range(len(message)):
-            if(int(message[i])-1 < 0 or int(message[i])-1 >= len(global_data.EXCHANGE_LIST)): #invalid number entry
-                send_message(phoneNumber,RESPONSE_DICT["invalidNum"]) #let user know
+        for i in range(len(message.body)):
+            if(int(message.body[i])-1 < 0 or int(message.body[i])-1 >= len(global_data.EXCHANGE_LIST)): #invalid number entry
+                send_message(message.from_,RESPONSE_DICT["invalidNum"]) #let user know
                 valid = False
                 break
             else:
-                exchanges.append(global_data.EXCHANGE_LIST[int(message[i])-1]) #add each exchange to list
+                exchanges.append(global_data.EXCHANGE_LIST[int(message.body[i])-1]) #add each exchange to list
         #user has sent valid message
         if(valid):
-            global_data.USER_DICT[phoneNumber] = (exchanges,global_data.USER_DICT[phoneNumber][1])
-            send_message(phoneNumber,RESPONSE_DICT["minArb"])
+            global_data.USER_DICT[message.from_] = (exchanges,global_data.USER_DICT[message.from_][1])
+            send_message(message.from_,RESPONSE_DICT["minArb"])
         return
 
-    elif(global_data.USER_DICT[phoneNumber][1] == 0.0):
+    elif(global_data.USER_DICT[message.from_][1] == 0.0):
         print "get arbitrage"
-        if(float(message)>100 or float(message<0)):
-            send_message(phoneNumber,RESPONSE_DICT["invalidNum"])
+        if(float(message.body)>100 or float(message.body<0)):
+            send_message(message.from_,RESPONSE_DICT["invalidNum"])
         else:
-            global_data.USER_DICT[phoneNumber] = (global_data.USER_DICT[phoneNumber][0],float(message)/100.0) #set customers min arb value
-            send_message(phoneNumber,RESPONSE_DICT["thank"])
+            global_data.USER_DICT[message.from_] = (global_data.USER_DICT[message.from_][0],float(message.body)/100.0) #set customers min arb value
+            send_message(message.from_,RESPONSE_DICT["thank"])
         return
 
     else:
         print "invalid command"
-        send_message(phoneNumber,RESPONSE_DICT["invalidCommand"])
+        send_message(message.from_,RESPONSE_DICT["invalidCommand"])
         return
 
 
 
 #Returns True if there is a new message
-def get_next_message():
-    messages = receive_messages() #get messages
-    if(len(messages) > currentMessage["index"]+1): #check if there is a new message
-        currentMessage["index"] += 1 #set new index for currentMessage
-        m = messages[currentMessage["index"]] #get next message
+#False otherwise
+def get_next_message(currentSid):
+    messages = client.messages.list(to="+17248061286") #get messages
 
-        #update currentMessage info
-        currentMessage["from"] = m.from_
-        currentMessage["body"] = m.body
-        return True
+    i=0
+    while(messages[i].sid != currentSid): #check if there is at least 1 new message
+        m = messages[i] #get next message
+        print "new sid "+m.sid
+        print "current sid "+currentSid
+        print m.body
+        print m.from_
+        handle_message(m)
+        time.sleep(1.5)
+        i += 1
 
+    currentSid = messages[0].sid
+    print "currentSid "+currentSid
+    return currentSid
 #testing
-'''
-currentMessage = {"index":-1,"sid":0,"from_":"+11111111111","body":"Some message"} #index of last message handled to check if there's a new message
+#currentSid = "SMec6b597e40d0e5d001a234c1db1f0da9"
 
-while(get_next_message() == True):
-    print currentMessage["body"]
-'''
-handle_message("+17248417560","Hello") #new phone number
-print global_data.USER_DICT
-handle_message("+17248417560","12") #set markets
-print global_data.USER_DICT
-
-handle_message("+17248417560","2") #set arbitrage
-print global_data.USER_DICT
-
-handle_message("+17248417560","reset") #reset values
-print global_data.USER_DICT
-"""
-handle_message("+17248417560","12") #set markets
-print global_data.USER_DICT
-handle_message("+17248417560","2") #set arbitrage
-print global_data.USER_DICT
-"""
-handle_message("+17248417560","stop") #delete user
-print global_data.USER_DICT
+# while (True):
+#     currentSid = get_next_message(currentSid) #new phone number
+#     print global_data.USER_DICT
