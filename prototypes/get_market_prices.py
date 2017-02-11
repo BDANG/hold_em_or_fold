2bb31b0ee7718e7233812fd2d4dfec4215f1fe24
BDANG <brindang776@aol.com>
2017-02-10 21:33:02 -0500

    working get_market_price(bitfinex)

--- prototypes/get_market_prices.py
+++ prototypes/get_market_prices.py
@@ -13,13 +13,11 @@ def get_bitfinex():
     for tdElement in tdList: #iterate the list of <td>
         if tdElement.text == "BTCUSD": #there are two: <td>BTCUSD</td>
             nextSiblingElement = tdElement.next_sibling.next_sibling
-            if ("\n" not in nextSiblingElement.text):
+            if ("\n" not in nextSiblingElement.text): #the desired element does not have a newline character in the next
                 marketPrice = float(nextSiblingElement.text)
 
-    #print spans
-    #print r
-
     return marketPrice
+    
 #The method will return the current market price for a given exchange
 #exchangeName - a string that specifies the exchange
 #returns the current market price as a float
